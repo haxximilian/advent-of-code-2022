69071b85f4a054d9cec537af4d0cdadf8f724fae
Maximilian Emmerik <maximilian.emmerik@gmail.com>
2022-12-06 13:10:01 +0100

    Day 5

---
+++ src/day05/index.ts
@@ -0,0 +1,109 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const getStacks = (rows: string[]) => {
+  const stacks: Array<string[]> = [];
+
+  rows.forEach((row) => {
+    for (let i = 0; i < row.length; i++) {
+      const element = row[i];
+
+      if (i % 4 === 1 && element.trim()) {
+        const rowIndex = (i - 1) / 4;
+
+        if (stacks[rowIndex]) {
+          stacks[rowIndex].push(element);
+        } else {
+          stacks[rowIndex] = [element];
+        }
+      }
+    }
+  });
+
+  return stacks;
+};
+
+const getTopRow = (
+  moves: string[],
+  stacks: Array<string[]>,
+  isCrateMover9000 = false,
+): string => {
+  moves.forEach((move) => {
+    const split = move.split(" ");
+    const amount = split[1];
+    const from = split[3];
+    const to = split[5];
+
+    let items: string[] = [];
+
+    for (let i = 0; i < +amount; i++) {
+      const item = stacks[+from - 1].shift();
+      if (item)
+        isCrateMover9000 ? items.push(item) : stacks[+to - 1].unshift(item);
+    }
+    if (isCrateMover9000) {
+      stacks[+to - 1] = [...items, ...stacks[+to - 1]];
+    }
+  });
+
+  return stacks.reduce((a, b) => {
+    return (a += b[0]);
+  }, "");
+};
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const rows = input.split("\n", 8);
+  const moves = input.split("\n").slice(10);
+  const stacks = getStacks(rows);
+
+  return getTopRow(moves, stacks);
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const rows = input.split("\n", 8);
+  const moves = input.split("\n").slice(10);
+  const stacks = getStacks(rows);
+
+  return getTopRow(moves, stacks, true);
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
+
+// const crates = input.slice(0, input.indexOf("\n\n"));
+// const moves = input
+//   .slice(input.indexOf("\n\n") + 2, input.length)
+//   .split("\n");
+
+// for (let i = 0; i < moves.length; i++) {
+//   const amount = moves[i].slice(5, 6);
+//   const from = moves[i].slice(12, 13);
+//   const to = moves[i].slice(17, 18);
+
+//   const row = crates.split("\n").map((row) => row.trim());
+//   console.log(row);
+//   console.log(amount, from, to);
+// }
